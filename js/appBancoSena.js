47c7e8b05ebef88114209e2720b7462834aa7b91
DuvanTIC2021 <duvancan_97@hotmail.com>
2021-08-26 21:57:33 -0500

    BancoSena

--- js/appBancoSena.js
+++ js/appBancoSena.js
@@ -1,9 +1,54 @@
-var app = new Vue({
+app = new Vue({
     el: "#app",
     data: {
-        
+        //definimos las variables que vamos a utilizar
+        saldo: 500000,
+        opcValor: 0,
+        valor: 0,
+        arrayValores: []
     },
     methods: {
-        
+        getValor() {
+            if (this.opcValor == "1") {
+                this.valor = 10000;
+            } else if (this.opcValor == "2") {
+                this.valor = 20000;
+            } else if (this.opcValor == "3") {
+                this.valor = 50000;
+            }
+        },
+        validarSaldo() {
+            if (this.valor > this.saldo) {
+                Swal.fire("Error en saldo!", "Fondos insuficientes!", "error");
+                return false;
+            } else {
+                return true;
+            }
+        },
+        procesar() {
+
+            var rta = this.validarSaldo();
+            if (rta) {
+                //   ok
+                this.retirarDinero();
+                this.agregarDetalle();
+            } else {
+                //   no hay plata
+            }
+        },
+        retirarDinero() {
+            this.saldo -= this.valor;
+            Swal.fire(
+                "Operación Exitosa!",
+                "Su dinero fue debitado correctamente su nuevo saldo es " + this.saldo,
+                "success"
+            );
+        },
+        agregarDetalle() {
+            this.arrayValores.push(this.valor);
+        }
+    },
+    computed: {
+
     },
 });
---
+++ js/retiro.js
@@ -0,0 +1,10 @@
+import data from '../js/app'
+var app = new Vue({
+    el: "#app",
+    data: {
+        //definimos las variables que vamos a utilizar
+        saldo: 0
+
+    },
+    methods: {},
+});
